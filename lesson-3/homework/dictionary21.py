809b976641df03880e30220c45f9150678020bfa
Doniyorbek <d.egamberdiyev@newuu.uz>
2025-03-09 18:11:40 +0500

    commited

--- lesson-3/homework/dictionary21.py
+++ lesson-3/homework/dictionary21.py
@@ -1,3 +1,3 @@
-dict21 = {'a': 3, 'b': 1, 'c': 2}
-sorted_by_value = dict(sorted(dict21.items(), key=lambda item: item[1]))
-print("Sorted by Value:", sorted_by_value)
+r = range(50, 100)
+for i in r:
+    print(i)
